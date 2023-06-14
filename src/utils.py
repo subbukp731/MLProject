e19e021e4cb48c4b2842930adf133598ed2698d8
Subramani K P <subbukp690@gmail.com>
2023-06-14 14:12:45 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import os
 import sys
-import dill
- 
-import numpy as np
+
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -16,13 +17,12 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-
-def evaluate_models(x_train, y_train,x_test,y_test,models,param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
@@ -31,16 +31,16 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
             para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
-            gs.fit(x_train,y_train)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(x_train,y_train)
+            model.fit(X_train,y_train)
 
-            #model.fit(x_train, y_train)  # Train model
+            #model.fit(X_train, y_train)  # Train model
 
-            y_train_pred = model.predict(x_train)
+            y_train_pred = model.predict(X_train)
 
-            y_test_pred = model.predict(x_test)
+            y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
 
@@ -49,6 +49,14 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
